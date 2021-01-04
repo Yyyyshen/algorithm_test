f9fc78a93152970c80e753afdf4a41db4ac4a754
Yyyyshen <Yyyyshen@gmail.com>
2021-01-04 14:45:51 +0800

    做些刷题计划

---
+++ leet_code_01/leet_code_01.cpp
@@ -0,0 +1,15 @@
+﻿// leet_code_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+/**
+ * 计划有空时在这个项目中刷题LeetCode
+ * 根据题大小，可能是5~10题一组（一个子项目）
+ */
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
