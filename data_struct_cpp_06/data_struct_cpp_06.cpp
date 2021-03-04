6973ddab7ed4316ad6bd43e780c6e6e873b8d2e9
Yyyyshen <Yyyyshen@gmail.com>
2021-03-04 16:00:42 +0800

    各种树基本概念，BST一些练习

--- data_struct_cpp_06/data_struct_cpp_06.cpp
+++ data_struct_cpp_06/data_struct_cpp_06.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <assert.h>
+#include <string>
 /**
  * 树
  */
@@ -76,13 +77,6 @@ public:
 	}
 	BinarySearchTree() {
 		root = NULL;
-	}
-	void inOrderPrint(Node* currentNode) {
-		if (currentNode != NULL) {
-			inOrderPrint(currentNode->leftChild);
-			cout << currentNode->value << endl;
-			inOrderPrint(currentNode->rightChild);
-		}
 	}
 	Node* getRoot() {
 		return root;
@@ -307,6 +301,302 @@ public:
 		}
 	}
 };
+/**
+ * AVL平衡树，是最佳情况的二叉搜索树
+ * 搜索复杂度为O(log(n))
+ *
+ * AVL插入方式与BST相同，但经常会导致不平衡，需要执行旋转
+ * 旋转右左旋和右旋
+ *
+Node U – an unbalanced node
+Node C – child node of node U
+Node G – grandchild node of node U
+四种情况：
+Left-Left: Node C is the left-child of Node U, and Node G is left-child of Node C
+Left-Right: Node C is the left-child of Node U, and Node G is right-child of Node C
+Right-Right: Node C is the right-child of Node U, and Node G is right-child of Node C
+Right-Left: Node C is right-child of Node U, and Node G is left-child of Node C
+
+删除节点同理
+算法删除
+1.删​​除给定的节点号
+删除给定的方法与删除BST中的方法相同。
+此时，树将变得不平衡，并且要重新平衡树，我们需要执行某种旋转（向左或向右）。
+首先，我们需要定义AVL树的结构以及相对于currentNode的一些节点，使用第一步将其插入。
+2.导线向上
+从给定的节点开始向上遍历，直到找到第一个不平衡节点。
+节点U-一个不平衡的节点
+节点C-节点U的子节点
+节点G-节点U的孙节点
+3.重新平衡树
+为了重新平衡树，我们将在子树上执行旋转，其中U是根节点。旋转有两种类型（左，右）。根据节点U，C和G的安排，我们遇到了四种不同的情况。
+左-左：节点C是节点U的左子节点，节点G是节点C的左子节点
+左右：节点C是节点U的左子节点，节点G是节点C的右子节点
+右右：节点C是节点U的右子节点，节点G是节点C的右子节点
+右左：节点C是节点U的右子节点，节点G是节点C的左子节点
+
+成功完成第一个不平衡节点U的旋转后，向上移动并找到下一个不平衡节点，并执行相同的一系列操作以进行平衡。
+继续平衡从第一个Node U到Node U祖先的不平衡节点，直到到达根为止。
+在那之后，我们将拥有一个遵循其属性的完全平衡的AVL树。
+
+ */
+int val_3 = 3;
+/**
+ * 红黑树
+ * 是自平衡二进制搜索树的另一种类型
+ * 但有一些附加功能：
+ * 红黑树中的节点被着色为红色或黑色。彩色节点有助于在插入或删除后重新平衡树
+ * 属性：
+ * 每个节点的颜色为红色或黑色
+ * 根始终是彩色的，黑色
+ * 两个红色节点不能相邻，即，没有红色父节点可以有一个红色子节点，反之亦然
+ * 从根到无的每个路径都包含相同数量的黑色节点
+ * NULL节点的颜色被认为是黑色
+ * 
+ * 对比AVL
+ * 尽管从技术上讲，AVL树比“红黑树”更“平衡”，但与红黑树相比，AVL树在插入和删除操作期间需要更多的旋转。
+ * 因此，如果搜索密集型应用程序不经常进行插入和删除，使用AVL树，否则使用红黑树。
+ */
+class Node_RB
+{
+	int value;
+	Node_RB* leftChild;
+	Node_RB* rightChild;
+	bool isRed;
+};
+/**
+ * 2-3树
+ * 2-3树是搜索树的另一种形式，但与BST不同，2-3树是平衡且有序的搜索树，它提供了一种非常有效的存储机制来保证快速操作
+ * 一项关键功能是，无论执行多少插入或删除操作，它都能保持平衡。
+ * 叶节点始终存在于同一级别上，并且数量很少。
+ * 这是为了确保高度不会增加到一定水平，因为所有操作的时间复杂度主要取决于高度。
+ * 理想情况下，我们希望高度以对数形式表示，因为随着树的变大，将需要更多时间来执行操作。
+ * 在2-3棵树中，树的高度是对数。
+ * 2-3-4树，每个节点可以容纳更多key，是2-3树的更高级形式
+ */
+int val_4 = 0;
+/**
+ * 取BST中最小值
+ */
+int findMin(Node* rootNode) {
+	// So keep traversing (in order) towards left till you reach leaf node,
+	//and then return leaf node's value
+	if (rootNode == NULL)
+		return -1;
+
+	while (rootNode->leftChild != NULL) {
+		rootNode = rootNode->leftChild;
+	}
+	return rootNode->value;
+}
+//递归
+int findMin_Re(Node* rootNode) {
+	// So keep traversing (in order) towards left till you reach leaf node,
+	//and then return leaf node's value
+	if (rootNode == NULL)
+		return -1;
+	else if (rootNode->leftChild == NULL)
+		return rootNode->value;
+	else
+		return findMin_Re(rootNode->leftChild);
+}
+/**
+ * 取BTS中第k个最小值
+ */
+class ArrayList {
+private:
+	int* arr;
+	int num_elements;
+	int capacity;
+public:
+	ArrayList(int size) {
+		arr = new int[size];
+		num_elements = 0;
+		capacity = size;
+	}
+
+	void insert(int val) {
+		if (num_elements <= capacity) {
+			arr[num_elements] = val;
+			num_elements++;
+		}
+		else {
+			resize();
+			arr[num_elements] = val;
+			num_elements++;
+		}
+	}
+
+	void resize() {
+		int* tempArr = new int[capacity * 2];
+		capacity *= 2;
+
+		for (int i = 0; i < num_elements; i++) {
+			tempArr[i] = arr[i];
+		}
+		delete arr;
+		arr = tempArr;
+	}
+
+	int length() {
+		return num_elements;
+	}
+
+	void print() {
+		for (int i = 0; i < num_elements; i++)
+			cout << arr[i] << " ";
+		cout << endl;
+	}
+
+	int getAt(int index) {
+		return arr[index];
+	}
+};
+//中序遍历有序填入数组
+void inOrderTraversal(Node* rootNode, ArrayList* result) {
+
+	if (rootNode != NULL) {
+
+		inOrderTraversal(rootNode->leftChild, result);
+		result->insert(rootNode->value);
+		inOrderTraversal(rootNode->rightChild, result);
+
+	}
+}
+//排序树，放入数组
+int findKthMax(Node* rootNode, int k, int nodes) {
+	if (k <= 0) {
+		return -1;
+	}
+	//Perform In-Order Traversal to get sorted array. (ascending order)
+	//Return value at index [length - k]
+	ArrayList* result = new ArrayList(nodes);
+	inOrderTraversal(rootNode, result);
+	return result->getAt(nodes - k);
+}
+//递归
+static int  counter = 0; //全局计数，找到最大值后往前遍历并计数
+Node* findKthMaxRecursive(Node* root, int k) {
+	if (root == nullptr || counter >= k) {
+		return nullptr;
+	}
+	//Recursively reach the right-most node (which has the highest value)
+	Node* node = findKthMaxRecursive(root->rightChild, k);
+
+	if (counter != k) {
+		//Increment counter if kth element is not found
+		counter++;
+		node = root;
+	}
+	//Base condition reached as kth largest is found
+	if (counter == k) {
+		return node;
+	}
+	else {
+		//Traverse left child if kth element is not reached
+		return findKthMaxRecursive(root->leftChild, k);
+	}
+}
+//used to call for the recursive algorithm
+int findKthMax_Re(Node* root, int k) {
+	Node* node = findKthMaxRecursive(root, k);
+	if (node != nullptr)
+		return node->value;
+	return -1;
+}
+/**
+ * 在BST中寻找指定节点所有祖先节点
+ */
+bool recur(Node* rootNode, int k, string* result) {
+
+	//base cases
+	if (rootNode == NULL)
+		return false;
+	else if (rootNode->value == k)
+		return true;
+	else {
+		//To check if target is present in either left or right subtree of currentNode
+		if (recur(rootNode->leftChild, k, result) || recur(rootNode->rightChild, k, result))
+		{
+			*(result) = *(result)+to_string(rootNode->value) + ",";
+			return true;
+		}
+	}
+
+	return false;
+}
+//递归
+string findAncestors(Node* rootNode, int k) {
+	string* result = new string;
+	recur(rootNode, k, result);
+	return *(result);
+}
+//迭代
+string findAncestors(Node* rootNode, int k) {
+
+	if (rootNode == NULL)
+		return "";
+
+	string ancestors = "";
+	Node* currentNode = rootNode;
+
+	while (currentNode != NULL) {
+		if (k > currentNode->value) {
+			ancestors += to_string(currentNode->value) + ",";
+			currentNode = currentNode->rightChild;
+		}
+		else if (k < currentNode->value) {
+			ancestors += to_string(currentNode->value) + ",";
+			currentNode = currentNode->leftChild;
+		}
+		else  // when k == currentNode.value
+			return ancestors;
+	}
+	return "";
+}
+/**
+ * 求BST高度
+ */
+int findHeight(Node* rootNode) {
+	if (rootNode == NULL)
+		return -1;
+	else {
+		// Find Height of left subtree and then right subtree
+		// Return greater height value of left or right subtree (plus 1)
+		int leftHeight = findHeight(rootNode->leftChild);
+		int rightHeight = findHeight(rootNode->rightChild);
+
+		if (leftHeight > rightHeight)
+			return leftHeight + 1;
+		else
+			return rightHeight + 1;
+	}
+}
+/**
+ * 找出与根节点距离为k的所有节点
+ */
+void findK(Node* root, int k, string* result) {
+
+	if (root == NULL)
+		return;
+
+	if (k == 0) {
+		*(result) = *(result)+to_string(root->value) + ",";
+	}
+	else {
+		//Decrement k at each step till you reach at the leaf node
+		// or when k == 0 then append the Node's data into result string
+		findK(root->leftChild, k - 1, result);
+		findK(root->rightChild, k - 1, result);
+	}
+}
+string findKNodes(Node* root, int k) {
+
+	string* result = new string;
+	findK(root, k, result);
+
+	return *(result);
+}
 
 int main()
 {
