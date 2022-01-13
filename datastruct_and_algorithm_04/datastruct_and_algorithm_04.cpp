d43c2b31e172f8f3400150ac77f1fd9998aa76c1
Yyyyshen <Yyyyshen@gmail.com>
2022-01-13 11:52:05 +0800

    第四阶段

---
+++ datastruct_and_algorithm_04/datastruct_and_algorithm_04.cpp
@@ -0,0 +1,17 @@
+﻿// datastruct_and_algorithm_04.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//第四阶段
+// BM、KMP、AC自动机
+// 红黑树
+// 哈希算法
+// 高级篇、实战篇
+//
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
