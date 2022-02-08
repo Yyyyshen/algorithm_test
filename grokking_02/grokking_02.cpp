d6f6c170c7738d0c2e21972c27a3dcda53bda7a1
Yyyyshen <Yyyyshen@gmail.com>
2022-02-08 20:36:38 +0800

    滑动窗口

--- grokking_02/grokking_02.cpp
+++ grokking_02/grokking_02.cpp
@@ -10,6 +10,12 @@ using namespace std;
 //
 //滑动窗口
 //
+//两点法
+//
+
+//=======================================================================
+
+//滑动窗口
 
 //例题：一个数组，求所有K个元素子序列的平均值
 class AverageOfSubarrayOfSizeK {
@@ -201,8 +207,154 @@ public:
 //
 
 
-//练习：
+//练习：给定字符串和一个模式串，找出字符串是否包含模式串的任何排列
+//分析，窗口大小固定，即模式串长度，使用map作为模式串统计
+class StringPermutation {
+public:
+	static bool findPermutation(const string& str, const string& pattern) {
+		unordered_map<char, int> map;
+		int window_start = 0, matched = 0;//记录已经匹配的字符
+		for (auto c : pattern)
+			++map[c];
+		for (int window_end = 0; window_end < str.size(); ++window_end)
+		{
+			char right = str[window_end];
+			//在模式串里，匹配到了，则计数
+			if (map.find(right) != map.end())
+				if (--map[right] == 0)
+					++matched;
 
+			//所有字符都匹配，则返回true
+			if (matched == map.size())
+				return true;
+
+			if (window_end >= pattern.size() - 1)
+			{
+				char start = str[window_start++];
+				//某字符从匹配变为不匹配
+				if (map.find(start) != map.end())
+					if (map[start]++ == 0)
+						--matched;
+			}
+		}
+		//时间复杂度O(N+M)，M为模式串长度，空间O(M)
+		return false;
+	}
+};
+
+//练习：给定字符串和一个模式串，找出模式串各排列在主串中出现的索引
+//分析，与上题类似，只不过每次匹配到，不直接返回，而是记录窗口初始位置
+class StringAnagrams {
+public:
+	static vector<int> findStringAnagrams(const string& str, const string& pattern) {
+		vector<int> resultIndices;
+		unordered_map<char, int> map;
+		int window_start = 0, matched = 0;
+		for (auto c : pattern)
+			++map[c];
+		for (int window_end = 0; window_end < str.size(); ++window_end)
+		{
+			char right = str[window_end];
+			if (map.find(right) != map.end())
+				if (--map[right] == 0)
+					++matched;
+			if (matched == map.size())
+				resultIndices.push_back(window_start);
+			if (window_end >= pattern.size() - 1)
+			{
+				char left = str[window_start++];
+				if (map.find(left) != map.end())
+					if (map[left]++ == 0)
+						--matched;
+			}
+		}
+		return resultIndices;
+	}
+};
+
+//练习：给定字符串和模式串，找出最小包含模式串所有字符的窗口
+//分析，还是与上面差不多的题目，只是匹配到之后，记录最小窗口
+class MinimumWindowSubstring {
+public:
+	static string findSubstring(const string& str, const string& pattern) {
+		unordered_map<char, int> map;
+		int window_start = 0, matched = 0;
+		int memo[2] = { -1 };//记录最小窗口索引
+		for (auto c : pattern)
+			++map[c];
+		for (int window_end = 0; window_end < str.size(); ++window_end)
+		{
+			char right = str[window_end];
+			if (map.find(right) != map.end())
+				if (--map[right] == 0)
+					++matched;
+
+			//只要符合，缩小窗口继续验证
+			while (matched == map.size())
+			{
+				if (memo[1] - memo[0] > window_end - window_start || memo[0] == -1)
+				{
+					memo[0] = window_start;
+					memo[1] = window_end;
+				}
+				char left = str[window_start++];
+				if (map.find(left) != map.end())
+					if (map[left]++ == 0)
+						--matched;
+			}
+		}
+		if (memo[0] == -1)
+			return "";
+		return str.substr(memo[0], memo[1] + 1);
+	}
+};
+
+//练习：给定字符串和单词列表，找到所有是单词列表串联而成子串的起始索引
+//分析，相当于字符的高级版，记录每个单词的出现频率，之后用现成的substr找子串，如果找到了所有单词，则记录一次结果
+class WordConcatenation {
+public:
+	static vector<int> findWordConcatenation(const string& str, const vector<string>& words) {
+		unordered_map<string, int> wordFrequencyMap;
+		for (auto word : words) {
+			wordFrequencyMap[word]++;
+		}
+
+		vector<int> resultIndices;
+		int wordsCount = words.size(), wordLength = words[0].length();
+
+		for (int i = 0; i <= int(str.length()) - wordsCount * wordLength; i++) {
+			unordered_map<string, int> wordsSeen;
+			for (int j = 0; j < wordsCount; j++) {
+				int nextWordIndex = i + j * wordLength;
+				// get the next word from the string
+				string word = str.substr(nextWordIndex, wordLength);
+				if (wordFrequencyMap.find(word) ==
+					wordFrequencyMap.end()) {  // break if we don't need this word
+					break;
+				}
+
+				wordsSeen[word]++;  // add the word to the 'wordsSeen' map
+
+				// no need to process further if the word has higher frequency than required
+				if (wordsSeen[word] > wordFrequencyMap[word]) {
+					break;
+				}
+
+				if (j + 1 == wordsCount) {  // store index if we have found all the words
+					resultIndices.push_back(i);
+				}
+			}
+		}
+
+		return resultIndices;
+	}
+};
+
+//=======================================================================
+
+//两点法
+
+//例：
 
 int main()
 {
