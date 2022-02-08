dd9aba06bda86e5c380c114be124bd884ad110b7
Yyyyshen <Yyyyshen@gmail.com>
2022-02-08 09:18:39 +0800

    总览

--- grokking_01/grokking_01.cpp
+++ grokking_01/grokking_01.cpp
@@ -3,11 +3,31 @@
 
 #include <iostream>
 
-/**
- * https://www.educative.io/
- * 算法课程
- * Grokking the Coding Interview: Patterns for Coding Questions
- */
+//
+//https://www.educative.io/
+// 算法课程
+// Grokking the Coding Interview: Patterns for Coding Questions
+//
+
+//
+//将大约150个问题映射成16种模式
+// 滑动窗口
+// 两点法
+// 快慢指针
+// 合并区间
+// 循环排序
+// 链表就地反转
+// 树的广度优先搜索 BFS
+// 树的深度优先搜索 DFS
+// 两堆
+// 子集
+// 二分查找变种
+// 按位异或
+// TOP-K问题
+// K路合并
+// 0-1背包（动态规划）
+// 拓扑排序（图）
+//
 
 int main()
 {
